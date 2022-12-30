66c8c9f8be50e7b4893ab50f8f381dcdd880ef94
shubhamdhadi <121656130+shubhamdhadi@users.noreply.github.com>
2022-12-29 23:04:55 -0700

    add all files

---
+++ c programe/area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int len,bre,area;
+	printf("\n enter len=");
+	scanf("%d,&len");
+	printf("\n enter bre=");
+	scanf("%d",&bre);
+	
+	area=len+bre;
+	printf("\n area of rectangle=%d",area);
+}
---
+++ c programe/area of square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int side,area;
+	printf("\n enter side=");
+	scanf("%d",&side);
+	
+	area=side*side;
+	printf("\n area of square=%d",area);
+}
