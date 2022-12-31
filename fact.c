26682fc55ca99182927ea35d4e6c8d22d33b0569
shubhamdhadi <121656130+shubhamdhadi@users.noreply.github.com>
2022-12-30 23:36:21 -0700

    add function is added

--- fact.c
+++ fact.c
@@ -10,4 +10,15 @@ fact=fact*i;
 }
 printf("\n fact=%d",fact);
   printf("welcome");
+  
+  
+  add();
+}
+void add()
+{
+  int a,b,c;
+  printf("\n enter a and b=");
+  scanf("%d%d",a,b);
+  c=a+b;
+  printf("\n add=%d",c);
 }
